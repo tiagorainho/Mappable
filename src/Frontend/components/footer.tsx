46768991dbfe984516d08a0d8fd9e19d3900e781
Tiago <tiago.rainho@ua.pt>
2022-10-12 16:21:35 +0100

    feat: add logos

--- src/Frontend/components/footer.tsx
+++ src/Frontend/components/footer.tsx
@@ -6,13 +6,13 @@ const Footer = () => (
     <footer className="px-6">
       <div className = "border-t-2 w-full flex flex-1 justify-center py-4">
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
           Aveiro Tech City - {' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          <span className="my-auto">
+            <Image src="/static/assets/images/brand.jpeg" alt="Vercel Logo" width={64} height={18} />
           </span>
         </a>
       </div>
