a3dad9534f18178a4bf5d732b8829bbff9d31e4c
Tiago <tiago.rainho@ua.pt>
2022-10-12 16:11:29 +0100

    feat: change generalization of models to decrease amount of code for each data model

--- src/Frontend/components/models/dataModel.tsx
+++ src/Frontend/components/models/dataModel.tsx
@@ -1,6 +1,6 @@
 
-import { Icon, IconOptions } from 'leaflet';
-import { ReactNode } from 'react';
+import { Icon } from 'leaflet';
+import { Marker, Popup } from 'react-leaflet';
 
 export const BASE_ICONS = "../../../static/assets/icons"
 
@@ -8,23 +8,20 @@ export interface IDataModel {
   id: string
   name: string
   icon: Icon
-}
-
-type Props = {
-  children?: ReactNode
+  coordinates: number[][]
 }
 
 export abstract class DataModel implements IDataModel {
   id: string
   name: string
   icon: Icon
-
-  abstract view: (model: any) => JSX.Element;
+  coordinates: number[][]
 
   constructor(
     id: string,
     name: string,
-    icon: string
+    icon: string,
+    coordinates: number[][],
   ) {
     this.id = id;
     this.name = name;
@@ -32,10 +29,51 @@ export abstract class DataModel implements IDataModel {
       iconUrl: `${BASE_ICONS}/${icon}`,
       iconSize: [25, 25]
     });
+    this.coordinates = coordinates
   }
 
   marker(): JSX.Element {
-    return this.view(this)
+    return GenericMarker(this)
   }
 
+  popup(coordinate: number[]): JSX.Element {
+    return GenericPopup({model: this, coordinate: coordinate})
+  }
+
+}
+
+export const GenericMarker = (model: DataModel) => {
+  return (
+    <>
+      {model.coordinates.map(coordinate => (
+        <Marker 
+          position={[coordinate[0], coordinate[1]]}
+          draggable={false}
+          icon={model.icon}
+        >
+          <GenericPopup model={model} coordinate={coordinate}></GenericPopup>
+        </Marker>
+      ))}
+    </>
+  )
+}
+
+type DataModelType = {
+  model: DataModel
+  coordinate: number[]
+}
+
+export const GenericPopup = (data: DataModelType) => {
+  return (
+    <>
+      <Popup>
+        <div className="text-gray-700">
+          <span><span className="font-semibold">ID: </span><span className="">{data.model.id}</span></span>
+        </div>
+        
+        <hr className="my-2"></hr>
+        {data.model.popup(data.coordinate)}  
+      </Popup>
+    </>
+  )
 }
