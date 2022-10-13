f869601ac50b0f0a16399ddbdba10876adaad1e9
Luisa <luisa.amaral9@gmail.com>
2022-10-13 18:41:48 +0100

    add script to move 10 buses

--- src/Backend/src/scripts/datagenerator.py
+++ src/Backend/src/scripts/datagenerator.py
@@ -8,7 +8,7 @@ if __name__=="__main__":
     # Opening JSON files
     museum = json.load(open(f'{DATA_FOLDER}/museum.json'))
     praca = json.load(open(f'{DATA_FOLDER}/pointOfInterest.json'))
-    bus = json.load(open(f'{DATA_FOLDER}/vehicle.json'))
+    
     # parkingGroup = json.load(open(f'{DATA_FOLDER}/parkingGroup.json'))
     parkingSpots = json.load(open(f'{DATA_FOLDER}/parkingSpots.json'))
 
@@ -21,9 +21,6 @@ if __name__=="__main__":
     print(response.status_code)
 
     response = requests.post(url, json=praca, headers={'content-type':'application/json'})
-    print(response.status_code)
-
-    response = requests.post(url, json=bus, headers={'content-type':'application/json'})
     print(response.status_code)
 
     # response = requests.post(url, json=parkingGroup, headers={'content-type':'application/json'})
