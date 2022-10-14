11fb179a88d7f160ad5724a7697a77af7f3234d0
Luisa <luisa.amaral9@gmail.com>
2022-10-14 11:12:24 +0100

    heat map done

--- src/Frontend/components/interactiveMap.tsx
+++ src/Frontend/components/interactiveMap.tsx
@@ -1,13 +1,15 @@
 import dynamic from "next/dynamic";
 
-const MapWithNoSSR = dynamic(() => import("../components/map"), {
-  ssr: false
-});
+const InteractiveMap = (props : any) => {
+    const MapWithNoSSR = dynamic(() => import("../components/" + props.map), {
+        ssr: false
+    });
 
-const InteractiveMap = () => (
-  <div className="h-96">
-    <MapWithNoSSR/>
-  </div>
-)
+    return (
+        <div className="h-96">
+            <MapWithNoSSR />
+        </div>
+    )
+}
 
 export default InteractiveMap
